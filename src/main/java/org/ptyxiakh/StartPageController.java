71b52bd4be290d0ff0b695c10c3b6a566a579eb9
Kwnstantinos <cocosjx95@gmail.com>
2021-05-23 12:28:19 +0300

    The user can get data from database and internet.

--- src/main/java/org/ptyxiakh/StartPageController.java
+++ src/main/java/org/ptyxiakh/StartPageController.java
@@ -2,15 +2,20 @@ package org.ptyxiakh;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.stage.Stage;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import java.io.IOException;
 import java.util.List;
 
 public class StartPageController
@@ -59,15 +64,48 @@ public class StartPageController
 //                System.out.println(s);
 //            }
         }
+        em.close();
+        emf.close();
     }
 
     public void InternetDataButtonClicked(ActionEvent event)
     {
-
+        try {
+            Stage stage = (Stage) e_button.getScene().getWindow();
+            stage.close();
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            primaryStage.setTitle("Chartistics");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
     public  void okButtonClicked(ActionEvent event)
     {
-        System.out.println(startpage_listView.getSelectionModel().getSelectedItem());
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("CHARTISTICS");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        //System.out.println("Selected data name:"+startpage_listView.getSelectionModel().getSelectedItem());
+        String name = startpage_listView.getSelectionModel().getSelectedItem();
+        Query query = em.createQuery("select d from Data d");
+        List<Data> list = query.getResultList();
+        //System.out.println("Mesaurments date for selected item");
+        for (Data data: list)
+            {
+                System.out.println("Data name:"+ data.getName());
+                if(data.getName().equals(name))
+                {
+                    List<Measurements> listMeasurements = data.getMeasurementsList();
+                    for (Measurements measurements : listMeasurements)
+                        System.out.println("Date:" + measurements.getDate() + "Value:" + measurements.getValue());
+                }
+            }
+        em.close();
+        emf.close();
     }
 }
